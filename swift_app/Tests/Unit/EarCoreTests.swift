6c4d8f78ccd2282fb141632b94221a254b8a3bd5
wanghan2022 <23766856@qq.com>
2026-04-17 11:14:49 +0800

    Revert "Add guitar spider crawl (爬格子) question generator in Ear module"
    This reverts commit f3dea563b15db8af40ed0df8b7b4e53f215c6c66.

--- swift_app/Tests/Unit/EarCoreTests.swift
+++ swift_app/Tests/Unit/EarCoreTests.swift
@@ -54,50 +54,6 @@ final class EarCoreTests: XCTestCase {
         XCTAssertFalse(score.isCorrect)
     }
 
-    func testSpiderCrawlGeneratorProducesFourChoices() {
-        var rng = SystemRandomNumberGenerator()
-        let q = SpiderCrawlQuestionGenerator.next(difficulty: .中级, using: &rng)
-        XCTAssertEqual(q.choices.count, 4)
-        XCTAssertTrue(q.choices.contains(q.answer), "选项中应包含正确答案")
-        XCTAssertEqual(q.difficulty, .中级)
-        XCTAssertFalse(q.promptZh.isEmpty)
-    }
-
-    func testSpiderAscendingPathCrossesToNextString() {
-        let path = SpiderCrawlQuestionGenerator.makePath(
-            pattern: .ascendingPerString,
-            blockStart: 5,
-            rounds: 2,
-            maxFret: 12
-        )
-        let i = path.firstIndex(where: { $0.stringIndexFromBass == 0 && $0.fret == 8 })!
-        XCTAssertEqual(path[i + 1].stringIndexFromBass, 1)
-        XCTAssertEqual(path[i + 1].fret, 5)
-    }
-
-    func testSpiderAscendingPathAdvancesBlockAfterSixStrings() {
-        let path = SpiderCrawlQuestionGenerator.makePath(
-            pattern: .ascendingPerString,
-            blockStart: 3,
-            rounds: 2,
-            maxFret: 12
-        )
-        let lastRound1 = SpiderCell(stringIndexFromBass: 5, fret: 6)
-        let firstRound2 = SpiderCell(stringIndexFromBass: 0, fret: 4)
-        XCTAssertEqual(path[6 * 4 - 1], lastRound1)
-        XCTAssertEqual(path[6 * 4], firstRound2)
-    }
-
-    func testSpiderBeginnerPresetOnlyAscending() {
-        let p = SpiderCrawlQuestionGenerator.preset(for: .初级)
-        XCTAssertEqual(p.allowedPatterns, [.ascendingPerString])
-    }
-
-    func testSpiderCellZhLabelUsesChineseStringName() {
-        let c = SpiderCell(stringIndexFromBass: 0, fret: 7)
-        XCTAssertEqual(c.zhLabel, "六弦 7品")
-    }
-
     func testEarSeedDecodingBankA() throws {
         let json = """
         {
